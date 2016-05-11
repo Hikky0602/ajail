723277a495b9a70ffa16d673b8583e10ae75f09a
sumtrue <inoue-kazuma@ed.tmu.ac.jp>
2016-05-11 14:35:19 +0900

    sum

--- shift_worker.php
+++ shift_worker.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html >
-<html>
-
-<head>
-<meta charset='utf8'>
-<meta http-equiv="content-style-type" content="text/css" />
-<link rel="stylesheet" type="text/css" href="shift_worker.css" />
-<?php
+<?
 require_once("calendar.php");
 require_once("database_class.php");
 require_once("schedule.php");
@@ -17,13 +10,186 @@ $day=num_month($year,$month);
 $name="小島瑠璃子";
 $user_id="kojiruri";
 
+$db=new database();
+$table="shift_submit";//テーブル名指定	
+
+$column="shift_data";
+$where=" user_id =".$user_id;
+$arr=$db->select($table,$column='', $where='');
+$arr=scheduleToArray($arr);
+	
+//提出された場合
+if(isset($_POST["schedule_worker"])){
+	
+//すでに提出されたシフトがある場合
+$column="shift_data";
+$where=" user_id ="."\"".$user_id."\"". " AND delete_flg=0 ";
+
+if(count($arr)>0){
+	//提出されてたら古いほうをlogical delete
+	$db->delete($table, $where);
+}
+
+$col="name,user_id,shift_year,shift_month,shift_data,delete_flg";//insertするcolumn指定
+$shift_data=implode("," , $_POST["schedule_worker"]);//insertするvalue指定
+$data="\"".$name."\""
+		.","
+		."\"".$user_id."\""
+		.","
+		.$year
+		.","
+		.$month
+		.","
+		."\"".$shift_data."\""
+		.","."0"
+		;
+echo $db->insert($table,$col,$data);
+
+}
+
+$arr= array();
 
 
 
 ?>
 
-<script >
+
+
+
+
+
+
+<!DOCTYPE html >
+<html>
+
+<head>
+<meta charset='utf8'>
+<meta http-equiv="content-style-type" content="text/css" />
+<link rel="stylesheet" type="text/css" href="shift_worker.css" />
+
+<!--?php
+
+require_once("calendar.php");
+require_once("database_class.php");
+require_once("schedule.php");
+
+$year=date("Y");
+$month=date("m");
+$day=num_month($year,$month);
+
+$name="小島瑠璃子";
+$user_id="kojiruri";
+?-->
+
+</head>
+
+
+
+
+<body >
+<div id="container">
+<div id="header">
+	名前:<?php echo $name; ?>
+	<br>
+	シフト締切:前月25日
+	<br>
+</div>
+
+<div id="schedule">
+	<?php echo  $year."年".$month. " 月"; ?>
+	
+	<div id="calender">
+		<table border="/">
+		<tr>
+			<th>日
+			<th>月
+			<th>火
+			<th>水
+			<th>木
+			<th>金
+			<th>土
+		</tr>
+
+
+
+		<form method ="post" action="" name="b1" class="squareBt">
+
+		
+		<!-- カレンダー表示 -->		
+		<tr>
+		<!--初月の曜日揃え-->
+<?php	for($j=0;$j<date_id($year,$month,1);$j++){	?>
+			<td> 
+		
+<?php	}for($j=1;$j<=7-date_id($year,$month,1);$j++){	?>
+			<td>
+			<input type="button" class=“squareBt”  value=<?php echo $j ?>&nbsp&nbsp onClick=turn(<?php echo ($j-1) ?>)> 
+<?php	}	?>
+		
+		</tr>
+		
+<!--	週ごとに改行	-->
+<?php		$cnt=0;
+		for($i=$j;$i<=$day;$i++){
+			if(($i-$j)%7==0){	?>
+		
+		<tr>
+<?php		}					?>
+			<td>
 <!--
+<?php/*		if($i<10){			?>
+				<input type="button" name="sche[]" value=<?php echo $i ?>&nbsp&nbsp onClick=turn(<?php ($i-1) ?>) class=“squareBt” >" ;
+<?php/*		}else{				*/?>
+-->				<input type="button" name="sche[]" value=<?php echo $i ?> onClick=turn(<?php echo ($i-1) ?>) class=“squareBt” >
+<?php	//	}
+			if(($i-$j)%7==6){	?>
+		</tr>
+<?php	}}						?>
+		
+		</form>
+		</table>
+		
+	</div>
+</div>
+
+
+<div id="buttonArea">
+
+<form action=""   >
+<input id ="input" type="button" value="前回のデータ読み込み" onClick="inputSchedule();">
+</form>
+
+
+<form method="post" action="" name="hb" >
+
+<?php
+$where=" user_id ="."\"".$user_id."\"";
+$arr=$db->select($table,$column, $where);
+$arr=scheduleToArray($arr);
+for($i=0;$i<$day;$i++){
+	if(count($arr)>0){
+		if($arr[$i]==1){	?>
+			<input type="hidden" name="schedule_worker[]" value=1>
+<?php	}else{				?>
+			<input type="hidden" name="schedule_worker[]" value=0>
+<?php	}
+	}else{					?>
+			<input type="hidden" name="schedule_worker[]" value=0>
+<?php	}}	?>
+
+	<input type="submit" name="submit" value="提出" /> 
+</form>
+
+<br>赤:空いてない
+<br>青:空いている
+
+</div>
+</div>
+<!-- END -->
+
+
+
+<script >
 function turn(element){
 //element番目のボタンの色を変更
 
@@ -57,165 +223,11 @@ function inputSchedule(){
 	
 }
 
-
-// -->
 </script>
 
 
 
-<meta charset="utf-8">
-<title>sumtrue</title>
-</head>
-<body >
-<div><div>
-名前:<?php echo $name; ?>
-<br>
-シフト締切:前月25日
-<br>
-</div>
-
-<div>
-<table border="/">
-<tr><?php echo  $year."年".$month. " 月"; ?>
-<th>日
-<th>月
-<th>火
-<th>水
-<th>木
-<th>金
-<th>土
-</tr>
-
-
-
-<form method ="post" action="" name="b1" class="squareBt">
-<?php
-
-$db=new database();
-$table="shift_submit";//テーブル名指定	
-
-$column="shift_data";
-$where=" user_id =".$user_id;
-$arr=$db->select($table,$column='', $where='');
-$arr=scheduleToArray($arr);
-	
-	//提出された場合
-	if(isset($_POST["schedule_worker"])){
-		
-		//すでに提出されたシフトがある場合
-		$column="shift_data";
-		$where=" user_id ="."\"".$user_id."\"". " AND delete_flg=0 ";
-		
-		if(count($arr)>0){
-			//提出されてたら古いほうをlogical delete
-			$db->delete($table, $where);
-		}
-		
-		$col="name,user_id,shift_year,shift_month,shift_data,delete_flg";//insertするcolumn指定
-		$shift_data=implode("," , $_POST["schedule_worker"]);//insertするvalue指定
-		$data="\"".$name."\""
-				.","
-				."\"".$user_id."\""
-				.","
-				.$year
-				.","
-				.$month
-				.","
-				."\"".$shift_data."\""
-				.","."0"
-				;
-		echo $db->insert($table,$col,$data);
-		
-	}
-
-	$arr= array();
-		
-	/**  カレンダー表示 **/		
-		echo "<tr>";
-		//初月の曜日揃え
-		for($j=0;$j<date_id($year,$month,1);$j++){
-			echo "<td>"." ";
-		}
-		
-		for($j=1;$j<=7-date_id($year,$month,1);$j++){
-			//echo "<td>".($j);
-			echo "<td>";
-			
-				echo '<input type="button" class=“squareBt”  value='.$j.'&nbsp&nbsp'.' onClick=turn('.($j-1).')    >' ;
-				
-		}
-		echo "</tr>";
-		
-		//週ごとに改行
-		$cnt=0;
-		for($i=$j;$i<=$day;$i++){
-			if(($i-$j)%7==0){
-				echo "<tr>";
-			}
-			echo "<td>";
-			if($i<10){
-				echo "<input type=\"button\" name=\"sche[]\" value=".$i."&nbsp&nbsp"." onClick=turn(".($i-1).") class=“squareBt” >" ;
-			}else{
-				 echo "<input type=\"button\" name=\"sche[]\" value=".$i." onClick=turn(".($i-1).") class=“squareBt” >" ;
-			}
-			if(($i-$j)%7==6){
-				echo "</tr>";
-			}
-		}	
-	
-?>
-</table>
-</form>
-</div><div>
-
-<form action=""   >
-<input id ="input" type="button" value="前回のデータ読み込み" onClick="inputSchedule();">
-</form>
-
-
-
-<form method="post" action="" name="hb" >
-<?php
-
-$db=new database();
-$table="shift_submit";//テーブル名指定	
-
-$column="shift_data";
-$where=" user_id ="."\"".$user_id."\"";
-$arr=$db->select($table,$column, $where);
-$arr=scheduleToArray($arr);
-for($i=0;$i<$day;$i++){
-	if(count($arr)>0){
-		
-		if($arr[$i]==1){
-			echo "<input type=\"hidden\" name=\"schedule_worker[]\" value=1>";
-		}else{
-			echo "<input type=\"hidden\" name=\"schedule_worker[]\" value=0>";
-		}
-	}else{
-		
-		echo "<input type=\"hidden\" name=\"schedule_worker[]\" value=0>";
-	}
-	
-}
-
-	
-?>
-<input type="submit" name="submit" value="提出" /> 
-</form>
-<input type="button" class="squareBt" value="test" />
-<br>赤:空いてない
-<br>青:空いている
-</div></div>
-
 </body>
 </html>
-
-
-
-
-
-
-
 
 
