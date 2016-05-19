b2e1bf310bcc3ca9c7c51cce5ec9747c50e3a96b
sumtrue <inoue-kazuma@ed.tmu.ac.jp>
2016-05-19 10:14:51 +0900

    make confirm_regist

--- index.php
+++ index.php
@@ -34,9 +34,9 @@ if(isset($_SESSION["USERID"])){	?>
 
 </div>
 
+<!---	画像loop表示	-->
 	<div id="topPic">
 		<div id="loop">
-
 		<ul>
 			<?php
 				foreach(glob('img/*.jpg') as $file){
@@ -47,11 +47,13 @@ if(isset($_SESSION["USERID"])){	?>
 	</div>
 
 	<div id="mainColumn">
-		<div id="border"><span id="borderTxt">オススメ</span></div>
+		<div id="border"><p id="borderTxt">オススメ</p></div>
 		<div><img id="osusume" src="osusume.jpg" width="400px" height="300px"></div>
-		<div id="border">地図</div>
-		<div>
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2535.602214537006!2d139.44800216427416!3d35.627776349208524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0xf993ae9d993f0a45!2z44GK44Gj44GR44GEIOawuOWxseW6lw!5e0!3m2!1sja!2sjp!4v1462498534360" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		
+<!---	google map埋め込み	-->
+		<div id="border"><p id="borderTxt">地図</p></div>
+		<div id="map">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2535.602214537006!2d139.44800216427416!3d35.627776349208524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0xf993ae9d993f0a45!2z44GK44Gj44GR44GEIOawuOWxseW6lw!5e0!3m2!1sja!2sjp!4v1462498534360" width="600" height="450" frameborder="0"  style="border:0" allowfullscreen></iframe>
 		</div>
 	</div>
 
@@ -59,13 +61,10 @@ if(isset($_SESSION["USERID"])){	?>
 	<div id="sideColumn">
 		<button class="reserveBt">ご予約はこちらから</button>
 	</div>
-	<div>
-		<button type="checkbox" class="NGbutton" value="test"></button>
-	</div>
 	
 	<div id="under"></div>
 
-</div><!--外-->
+</div>
 </body>
 </html>
 
---
+++ shiftManagerProto/shift_worker_proto.php
@@ -0,0 +1,54 @@
+<?php
+
+define('TABLE','user_ploto');
+define('HOST','localhost');
+define('DB','Akifarm_db');
+define('USER','user');
+define('PASS','password');
+
+require_once("database_class.php");
+require_once("calendar.php");
+
+$link = mysqli_connect('localhost','user','password','Akifarm_db');
+
+if(isset($_POST['send'])===true){
+	$id = $_POST['id'];
+	$quary = 'SELECT * FROM user_ploto WHERE id="' . $id . '"';	
+	$res = mysqli_query($link, $quary);
+	$data = mysqli_fetch_assoc($res);
+	if($data==NULL){
+		echo 'ID error!';
+		include_once("shift_worker_login_proto.html");
+	}else{
+		setCookie("id", $id);
+		setCookie("name", $data['name']);
+		include_once("shift_worker_send_proto.php");
+	}
+}
+if(isset($_POST['month'])){
+include_once("shift_worker_send_proto.php");
+}
+
+if(isset($_POST["submit"])){
+	echo "提出完了";
+	$id = $_COOKIE["id"];
+	$month = $_POST['month'];
+
+	$schedule_post=array();
+	for($i=0;$i<30;$i++){
+		$schedule_post[$i]=0;
+	}
+	for($i=0;$i<count($_POST["schedule"]);$i++){
+		$schedule_post[$_POST["schedule"][$i]]=1;
+	}
+	var_dump($schedule_post);
+
+	$data=implode("," , $schedule_post);
+	$query='update shift_submit_proto set shift_data="'.$data.'" where id="'.$id.'" and month="'.$month.'"';
+	
+	mysqli_query($link, $query);
+
+		include_once("shift_worker_send_proto.php");
+}
+
+?>
