e3f80809dffdfd0c2fbbf48f36e360f4090f0ff4
sumtrue <inoue-kazuma@ed.tmu.ac.jp>
2016-05-19 10:37:31 +0900

    add DB作成書

--- shiftmanage02/shift_manager_proto.php
+++ shiftmanage02/shift_manager_proto.php
@@ -1,4 +1,34 @@
 <?php
+$yearNow = date("Y");
+$monthNow = date("m");
+$count = 0;
+
+$month = $monthNow;
+$year = $yearNow;
+
+if(isset($_POST["month"])){
+
+	$monthPost = $_POST["month"][0];
+
+	if($month-$monthPost>3){
+		$year = $yearNow + 1;
+		$month = $monthPost;
+	}else if($monthPost==12 && $month==1){
+		$year = $yearNow - 1;
+		$month = $monthPost;
+	}else{
+		$month = $monthPost;
+	}
+}else{
+	$monthPost = $month;
+}
+$prev = $month!=1 ? $monthPost-1 : 12;
+$next = $month!=12 ? $monthPost+1 : 1;
+	
+$day = num_month($year,$month);
+$startDay = date_id($year, $month, 1);
+
+
 $link = mysqli_connect('localhost','user', 'password','Akifarm_db');
 $query = 'select u.id, u.name, s.shift_data from user_ploto u join shift_submit_proto s on u.id = s.id'; 
 $res = mysqli_query($link, $query);
@@ -13,8 +43,9 @@ $res = mysqli_query($link, $query);
 </head>
 <body>
 
+<div><div>
 
-
+</div>
 <div>
 <table border=1>
 <tr><td>name</td>
